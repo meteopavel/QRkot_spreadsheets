b89b5b45a63148c22f6bdf9e07494409b97bb236
meteopavel <isu@meteopavel.ru>
2024-04-24 12:08:40 +0800

    Config Fixes

--- app/core/config.py
+++ app/core/config.py
@@ -4,12 +4,23 @@ from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str = 'QRKot'
-    description: str = 'Фонд помощи котикам'
+    app_title: str = 'QRKot_spreadsheets'
+    description: str = 'Фонд помощи котикам c отчётом в Google-таблице'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'secret'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email_user: Optional[str] = None
 
     class Config:
         env_file = '.env'
