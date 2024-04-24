d57a0804caccc3f26635e6d199764692683a1d64
meteopavel <isu@meteopavel.ru>
2024-04-24 12:18:38 +0800

    Add google_api Endpoint

---
+++ app/api/endpoints/google_api.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from aiogoogle import Aiogoogle
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import get_async_session
+from app.core.google_client import get_service
+from app.core.user import current_superuser
+
+from app.crud.charity_project import charity_crud
+
+from app.services.google_api import (
+    set_user_permissions, spreadsheets_create, spreadsheets_update_value
+)
+
+router = APIRouter()
+
+
+@router.post(
+    '/',
+    response_model=list[dict[str, int]],
+    dependencies=[Depends(current_superuser)],
+)
+async def get_report(
+        from_reserve: datetime,
+        to_reserve: datetime,
+        session: AsyncSession = Depends(get_async_session),
+        wrapper_services: Aiogoogle = Depends(get_service)
+):
+    """Только для суперюзеров."""
+    reservations = await charity_crud.get_count_res_at_the_same_time(
+        from_reserve, to_reserve, session
+    )
+    spreadsheetid = await spreadsheets_create(wrapper_services)
+    await set_user_permissions(spreadsheetid, wrapper_services)
+    await spreadsheets_update_value(spreadsheetid,
+                                    reservations,
+                                    wrapper_services)
+    return reservations
